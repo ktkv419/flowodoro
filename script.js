89aed1ffee801c6314a81da2cbabfb8c013c82ad
Pasha <kutikovpasha@gmail.com>
2023-03-25 14:47:29 +0600

    New menu animation. Started marking up timer window

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ btnMode.forEach((el) => el.addEventListener('click', changeScreen));
 
 const screenMode = [...document.querySelectorAll('.mode')];
 function changeScreen(e) {
+  screenMode.forEach((el) => el.classList.remove('active'));
+  const mode = e.target.closest('.user-nav__icon-box');
+
+  const modeIcon = mode.querySelectorAll('.user-nav__icon')[0];
+  modeIcon.classList.add('change');
+  setTimeout(() => modeIcon.classList.remove('change'), 500);
   screenMode.forEach((el) => el.classList.add('hidden'));
-  const mode = e.target.closest('.user-nav__icon-box').id;
-  screenMode.find((el) => el.id == `${mode}-screen`).classList.remove('hidden');
+  const screen = screenMode.find((el) => el.id == `${mode.id}-screen`);
+  screen.classList.add('active');
+  screen.classList.remove('hidden');
 }
