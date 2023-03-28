03a4ad25aef449f3bf61254b3a829ae0df02b125
ktkv419 <kutikovpasha@gmail.com>
2023-03-28 17:54:33 +0600

    Wrapping application in Class property for scope purposes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// THROW AWAY CODE
+///////////////////////////////////// THROW AWAY CODE
 const btnMode = document.querySelectorAll('.user-nav__icon-box');
 btnMode.forEach((el) => el.addEventListener('click', changeScreen));
 
@@ -8,10 +8,43 @@ function changeScreen(e) {
   const mode = e.target.closest('.user-nav__icon-box');
 
   const modeIcon = mode.querySelectorAll('.user-nav__icon')[0];
-  modeIcon.classList.add('change');
-  setTimeout(() => modeIcon.classList.remove('change'), 500);
+  // modeIcon.classList.add('change');
+  // setTimeout(() => modeIcon.classList.remove('change'), 500);
   screenMode.forEach((el) => el.classList.add('hidden'));
   const screen = screenMode.find((el) => el.id == `${mode.id}-screen`);
   screen.classList.add('active');
   screen.classList.remove('hidden');
 }
+//////////////////////////////////////////////////////
+
+class App {
+  changeTimerBtns = document.querySelector('.timer-screen__buttons');
+  changeModeBtns = document.querySelector('.user-nav');
+  constructor() {
+    this.#init();
+  }
+
+  #init() {
+    this.changeTimerBtns.addEventListener('click', this.changeTimePresets);
+    this.changeModeBtns.addEventListener('click', this.changeMode.bind(this));
+  }
+
+  changeTimePresets(e) {
+    if (!e.target.classList.contains('btn')) return;
+    console.log(+e.target.dataset.value);
+  }
+
+  changeMode(e) {
+    const modeEl = e.target.closest('.user-nav__icon-box');
+    const iconEl = modeEl.children[0];
+
+    iconEl.classList.add('change');
+    setTimeout(() => iconEl.classList.remove('change'), 500);
+
+    // [...this.changeModeBtns.children].filter((el) => el.id != modeEl.id).forEach(el => {
+    //   el.classList.remove
+    // })
+  }
+}
+
+const app = new App();
